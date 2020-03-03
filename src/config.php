8e39936d0f73b07f9b3c509343bb5dec93358383
jinyicheng <jinyicheng@live.com>
2020-03-03 14:13:37 +0800

    允许MIUI、EMUI、Flyme系统设备离线转为系统下发,新增配置支持

--- src/config.php
+++ src/config.php
@@ -5,5 +5,7 @@ return [
     'ios_app_master_secret' => '',
     'android_app_key' => '',
     'android_app_master_secret' => '',
-    'production_mode' => env('UMENG_MODE', false)
+    'production_mode' => env('UMENG_MODE', false),
+    'mi_push'=>false,
+    'mi_activity'=>''
 ];
